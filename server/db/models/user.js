2605d562bd4b337224299ffa92252410e5db39a8
Justin Cook <justin.cook.dev@gmail.com>
2019-09-05 10:32:12 -0700

    User model w/ basic fields.

---
+++ server/db/models/user.js
@@ -0,0 +1,54 @@
+import { Model, STRING } from 'sequelize';
+import db from '../connection';
+
+class User extends Model {}
+User.init(
+  {
+    firstName: {
+      type: STRING,
+      allowNull: false,
+      validate: {
+        notEmpty: true,
+      },
+    },
+    lastName: {
+      type: STRING,
+      allowNull: false,
+      validate: {
+        notEmpty: true,
+      },
+    },
+    username: {
+      type: STRING,
+      allowNull: false,
+      unique: true,
+      validate: {
+        notEmpty: true,
+      },
+    },
+    email: {
+      type: STRING,
+      allowNull: false,
+      unique: true,
+      validate: {
+        notEmpty: true,
+        isEmail: true,
+      },
+    },
+    password: {
+      type: STRING,
+      // leave true b/c Oauth will not have password
+      allowNull: true,
+      validate: {
+        notEmpty: true,
+        len: [8],
+        // passwords must be at least 8 characters with one letter and one number
+        is: /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/
+      },
+    },
+  },
+  {
+    sequelize: db,
+    modelName: 'user',
+  }
+);
