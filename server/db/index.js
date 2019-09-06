64ffc91f3bec5a3dd22266da00ecb7165677aeca
katherinepeterson <katherine.peterson@yahoo.com>
2019-09-05 21:10:53 -0700

    deleted oauthprovider model after conversationw tith team

--- server/db/index.js
+++ server/db/index.js
@@ -2,12 +2,10 @@ const db = require('./connection');
 const User = require('./models/user');
 const Provider = require('./models/Provider')
 const UserProvider = require('./models/UserProvider')
-const OauthProvider = require('./models/OauthProvider')
 
 module.exports = {
   db,
   User,
   Provider,
   UserProvider,
-  OauthProvider,
 };
