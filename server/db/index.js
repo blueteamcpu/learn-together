e231024cfd63842a41e7205422aedf38347af5d1
Justin Cook <justin.cook.dev@gmail.com>
2019-09-05 22:03:17 -0700

    Fixed minor typo in userProvider. Brought file naming conventions into sync.

--- server/db/index.js
+++ server/db/index.js
@@ -1,7 +1,7 @@
 const db = require('./connection');
 const User = require('./models/user');
-const Provider = require('./models/Provider')
-const UserProvider = require('./models/UserProvider')
+const Provider = require('./models/provider')
+const UserProvider = require('./models/userProvider')
 const Session = require('./models/session');
 
 module.exports = {
