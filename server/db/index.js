1f650ca59d3ead919c558ebebdabe5c1feebc6e4
katherinepeterson <katherine.peterson@yahoo.com>
2019-09-05 21:01:50 -0700

    added models to index.js file in db

--- server/db/index.js
+++ server/db/index.js
@@ -1,7 +1,13 @@
 const db = require('./connection');
 const User = require('./models/user');
+const Provider = require('./models/Provider')
+const UserProvider = require('./models/UserProvider')
+const OauthProvider = require('./models/OauthProvider')
 
 module.exports = {
   db,
   User,
+  Provider,
+  UserProvider,
+  OauthProvider,
 };
