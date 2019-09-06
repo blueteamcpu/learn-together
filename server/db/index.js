59b984b0c77570ba465c692d5dd8f20e9055484e
CB6670 <connerstennett@gmail.com>
2019-09-06 15:26:54 -0700

    Added event and group to connection

--- server/db/index.js
+++ server/db/index.js
@@ -3,6 +3,11 @@ const User = require('./models/user');
 const Provider = require('./models/Provider')
 const UserProvider = require('./models/UserProvider')
 const Session = require('./models/session');
+const Comment = require('./models/comment');
+const Post = require('./models/post');
+const Course = require('./models/course');
+const Event = require('./models/event');
+const Group = require('./models/group');
 
 module.exports = {
   db,
@@ -10,4 +15,9 @@ module.exports = {
   Provider,
   UserProvider,
   Session,
+  Comment,
+  Post,
+  Course,
+  Event,
+  Group
 };
