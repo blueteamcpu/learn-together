5ae742438428d385074e0f949b16bf48e70770e3
Nicholas Napoli <nicknapoli82@gmail.com>
2019-09-05 18:16:04 -0700

    Fix simple spelling mistake - checking my account and push

--- server/app/index.js
+++ server/app/index.js
@@ -1,14 +1,14 @@
 const path = require('path');
 const express = require('express');
 const morgan = require('morgan');
-const compressoin = require('compression');
+const compression = require('compression');
 const helmet = require('helmet');
 
 const app = express();
 
 app.use(morgan('dev'));
 app.use(helmet());
-app.use(compressoin());
+app.use(compression());
 
 app.use(express.static(path.join(__dirname, '..', '..', 'public')));
 
