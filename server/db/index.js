c8ca62974c65604c66ac18ce70453649eb8906cc
Justin Cook <justin.cook.dev@gmail.com>
2019-09-06 08:55:06 -0700

    Changed file path to Provider model.

--- server/db/index.js
+++ server/db/index.js
@@ -1,6 +1,6 @@
 const db = require('./connection');
 const User = require('./models/user');
-const Provider = require('./models/Provider');
+const Provider = require('./models/provider');
 const UserProvider = require('./models/userProvider');
 const Session = require('./models/session');
 const Comment = require('./models/comment');
