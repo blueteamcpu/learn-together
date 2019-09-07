ed887635161c81824ff814741fc7a8f84698935b
katherinepeterson <katherine.peterson@yahoo.com>
2019-09-07 13:41:56 -0700

    replaced homepage content with more relevant wording

--- client/components/Home/HomeDumb.js
+++ client/components/Home/HomeDumb.js
@@ -30,7 +30,7 @@ const HomepageHeading = ({ mobile }) => (
   <Container text>
     <Header
       as="h1"
-      content="Imagine-a-Company"
+      content="Learn Together"
       inverted
       style={{
         fontSize: mobile ? '2em' : '4em',
@@ -41,7 +41,7 @@ const HomepageHeading = ({ mobile }) => (
     />
     <Header
       as="h2"
-      content="Do whatever you want when you want to."
+      content="Find a community of people with the same learning goals."
       inverted
       style={{
         fontSize: mobile ? '1.5em' : '1.7em',
@@ -98,9 +98,7 @@ class DesktopContainer extends Component {
                 <Menu.Item as="a" active>
                   Home
                 </Menu.Item>
-                <Menu.Item as="a">Work</Menu.Item>
-                <Menu.Item as="a">Company</Menu.Item>
-                <Menu.Item as="a">Careers</Menu.Item>
+                <Menu.Item as="a">Explore</Menu.Item>
                 <Menu.Item position="right">
                   <Button as="a" inverted={!fixed}>
                     Log in
@@ -158,9 +156,7 @@ class MobileContainer extends Component {
           <Menu.Item as="a" active>
             Home
           </Menu.Item>
-          <Menu.Item as="a">Work</Menu.Item>
-          <Menu.Item as="a">Company</Menu.Item>
-          <Menu.Item as="a">Careers</Menu.Item>
+          <Menu.Item as="a">Explore</Menu.Item>
           <Menu.Item as="a">Log in</Menu.Item>
           <Menu.Item as="a">Sign Up</Menu.Item>
         </Sidebar>
@@ -217,99 +213,29 @@ class Home extends Component {
     return (
       <ResponsiveContainer>
         <Segment style={{ padding: '8em 0em' }} vertical>
-          <Grid container stackable verticalAlign="middle">
+          <Grid container stackable verticalAlign="middle" textAlign='center'>
             <Grid.Row>
               <Grid.Column width={8}>
                 <Header as="h3" style={{ fontSize: '2em' }}>
-                  We Help Companies and Companions
+                  We Help People Learn Together
                 </Header>
                 <p style={{ fontSize: '1.33em' }}>
-                  We can give your company superpowers to do things that they
-                  never thought possible. Let us delight your customers and
-                  empower your needs... through pure data analytics.
+                  We help you connect with people locally that are pursuing similar educational goals. 
                 </p>
                 <Header as="h3" style={{ fontSize: '2em' }}>
-                  We Make Bananas That Can Dance
+                  Online Education Doesn't Have to be a Solo Endeavor
                 </Header>
                 <p style={{ fontSize: '1.33em' }}>
-                  Yes that's right, you thought it was the stuff of dreams, but
-                  even bananas can be bioengineered.
+                  Find a community of people taking courses from your favorite online educational platform such as KhanAcademy and EdX.
                 </p>
-              </Grid.Column>
-              <Grid.Column floated="right" width={6}>
-                <Image
-                  bordered
-                  rounded
-                  size="large"
-                  src="/images/wireframe/white-image.png"
-                />
               </Grid.Column>
             </Grid.Row>
             <Grid.Row>
               <Grid.Column textAlign="center">
-                <Button size="huge">Check Them Out</Button>
+                <Button size="huge">Check It Out</Button>
               </Grid.Column>
             </Grid.Row>
           </Grid>
-        </Segment>
-        <Segment style={{ padding: '0em' }} vertical>
-          <Grid celled="internally" columns="equal" stackable>
-            <Grid.Row textAlign="center">
-              <Grid.Column style={{ paddingBottom: '5em', paddingTop: '5em' }}>
-                <Header as="h3" style={{ fontSize: '2em' }}>
-                  "What a Company"
-                </Header>
-                <p style={{ fontSize: '1.33em' }}>
-                  That is what they all say about us
-                </p>
-              </Grid.Column>
-              <Grid.Column style={{ paddingBottom: '5em', paddingTop: '5em' }}>
-                <Header as="h3" style={{ fontSize: '2em' }}>
-                  "I shouldn't have gone with their competitor."
-                </Header>
-                <p style={{ fontSize: '1.33em' }}>
-                  <Image avatar src="/images/avatar/large/nan.jpg" />
-                  <b>Nan</b> Chief Fun Officer Acme Toys
-                </p>
-              </Grid.Column>
-            </Grid.Row>
-          </Grid>
-        </Segment>
-        <Segment style={{ padding: '8em 0em' }} vertical>
-          <Container text>
-            <Header as="h3" style={{ fontSize: '2em' }}>
-              Breaking The Grid, Grabs Your Attention
-            </Header>
-            <p style={{ fontSize: '1.33em' }}>
-              Instead of focusing on content creation and hard work, we have
-              learned how to master the art of doing nothing by providing
-              massive amounts of whitespace and generic content that can seem
-              massive, monolithic and worth your attention.
-            </p>
-            <Button as="a" size="large">
-              Read More
-            </Button>
-            <Divider
-              as="h4"
-              className="header"
-              horizontal
-              style={{ margin: '3em 0em', textTransform: 'uppercase' }}
-            >
-              <a href="#">Case Studies</a>
-            </Divider>
-            <Header as="h3" style={{ fontSize: '2em' }}>
-              Did We Tell You About Our Bananas?
-            </Header>
-            <p style={{ fontSize: '1.33em' }}>
-              Yes I know you probably disregarded the earlier boasts as
-              non-sequitur filler content, but it's really true. It took years
-              of gene splicing and combinatory DNA research, but our bananas can
-              really dance.
-            </p>
-            <Button as="a" size="large">
-              I'm Still Quite Interested
-            </Button>
-          </Container>
         </Segment>
         <Segment inverted vertical style={{ padding: '5em 0em' }}>
           <Container>
@@ -320,17 +246,6 @@ class Home extends Component {
                   <List link inverted>
                     <List.Item as="a">Sitemap</List.Item>
                     <List.Item as="a">Contact Us</List.Item>
-                    <List.Item as="a">Religious Ceremonies</List.Item>
-                    <List.Item as="a">Gazebo Plans</List.Item>
-                  </List>
-                </Grid.Column>
-                <Grid.Column width={3}>
-                  <Header inverted as="h4" content="Services" />
-                  <List link inverted>
-                    <List.Item as="a">Banana Pre-Order</List.Item>
-                    <List.Item as="a">DNA FAQ</List.Item>
-                    <List.Item as="a">How To Access</List.Item>
-                    <List.Item as="a">Favorite X-Men</List.Item>
                   </List>
                 </Grid.Column>
                 <Grid.Column width={7}>
