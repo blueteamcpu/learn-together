97bc45cc3061d460a1419ac057dbe7f923b74bfb
Justin Cook <justin.cook.dev@gmail.com>
2019-09-07 11:23:55 -0700

    Change force to false.

--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ const { db } = require('./db/index');
 
 const PORT = process.env.PORT || 3000;
 
-db.sync({ force: true })
+db.sync({ force: false })
   .then(() => {
     app.listen(PORT, () => console.log(`App listening on port ${PORT}`));
   })
