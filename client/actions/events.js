a46b7cc60cf13465b38b866c0d5f72c9011ad5dc
katherinepeterson <katherine.peterson@yahoo.com>
2019-09-08 17:14:15 -0700

    set up event reducer

---
+++ client/actions/events.js
@@ -0,0 +1,10 @@
+// ACTION TYPES
+export const GET_EVENTS = 'GET_EVENTS';
+export const CREATE_EVENT = 'CREATE_EVENT';
+export const DELETE_EVENT = 'DELETE_EVENT';
+
+// ACTIONS
+
+
+// THUNKS
+
--- client/reducers/index.js
+++ client/reducers/index.js
@@ -1,6 +1,8 @@
 import { combineReducers } from 'redux';
 import authentication from './authentication';
+import events from './events';
 
 export default combineReducers({
   authentication,
+  events,
 });
