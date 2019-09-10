d13a1c0942badd869f46f44d8a569032529e6a7f
katherinepeterson <katherine.peterson@yahoo.com>
2019-09-09 18:52:36 -0700

    event api routes

--- client/actions/events.js
+++ client/actions/events.js
@@ -7,4 +7,17 @@ export const DELETE_EVENT = 'DELETE_EVENT';
 
 
 // THUNKS
-
+export const createEvent = (event) => async (dispatch, _, axios) => {
+    try {
+      const ev = await axios.post('/newevent', event);
+  
+      if (ev) {
+            dispatch({
+                type: CREATE_EVENT,
+                ev
+            });
+        }
+    } catch (error) {
+      console.error(error);
+    }
+  };
---
+++ client/components/CreateEventForm/CreateEventFormDumb.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class CreateEventForm extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {  }
+    }
+    render() { 
+        return ( 
+            <hr/>
+         );
+    }
+}
+ 
+export default CreateEventForm;
