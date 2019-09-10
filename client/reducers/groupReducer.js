f5825ebcc3a761893c0609378caa7be8173d37b7
Nicholas Napoli <nicknapoli82@gmail.com>
2019-09-09 22:54:26 -0700

    Create Group Form made - Need to start testing everything

--- client/reducers/groupReducer.js
+++ client/reducers/groupReducer.js
@@ -1,14 +1,20 @@
 import axios from 'axios';
 
 const SET_GROUPS = 'SET_GROUPS';
+const SET_DETAIL_GROUP = 'SET_DETAIL_GROUP';
 const FAILED_GROUPS_GET = 'FAILED_GROUPS_GET';
 
-export const getGroupList = (dispatch) => {
+// Eventually we will want to start passing the section here. Just not now
+export const getGroupList = (section = 0) => (dispatch) => {
   const groupList = axios.get('/group/all')
         .then(data => {
 	  dispatch({ type: SET_GROUPS, newGroups: data });
 	})
         .catch(e => dispatch({ type: FAILED_GROUPS_GET, groupGetFailed: true }));
+};
+
+export const postNewGroup = (group) => (dispatch) => {
+  dispatch({ type: SET_DETAIL_GROUP, group});
 };
 
 // Presumabely we will need some sort of tracking thing which manages the groups
@@ -25,8 +31,11 @@ export default (state = initialState, action) => {
   case SET_GROUPS: {
     return { ...state, groupList: [...state.groupList, action.newGroups] };
   }
+  case SET_DETAIL_GROUP: {
+    return { ...state, groupDetailed: action.group};
+  }
   case FAILED_GROUPS_GET: {
-    return { ...state, greoupGetFailed: action.groupGetFailed };
+    return { ...state, groupGetFailed: action.groupGetFailed };
   }
   default:
     return { ...state };
