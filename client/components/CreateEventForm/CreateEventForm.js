dc297a02fac7a5487ce32c9df183bcb529946b1a
katherinepeterson <katherine.peterson@yahoo.com>
2019-09-10 12:36:55 -0700

    syntax errors

--- client/components/CreateEventForm/CreateEventForm.js
+++ client/components/CreateEventForm/CreateEventForm.js
@@ -2,11 +2,11 @@ import { connect } from 'react-redux';
 import CreateEventForm from './LoginFormDumb';
 import { createEvent as _createEvent } from '../../actions/authentication';
 
-const mapDispatchToProps = (dispatch) => {
+const mapDispatchToProps = (dispatch) => ({
     createEvent(event) {
         dispatch(_createEvent(event));
     }
-}
+});
 
 export default connect(
   null,
--- server/app/index.js
+++ server/app/index.js
@@ -19,11 +19,8 @@ app.use(express.urlencoded({ extended: true }));
 app.use(require('./serializeUserMiddleware'));
 
 app.use('/auth', require('../auth/index'));
-<<<<<<< HEAD
 app.use('/event', require('../apiRoutes/event'));
-=======
 app.use('/group', require('../apiRoutes/group/group'));
->>>>>>> e2fe4e6963f2b6390a9f3af884160892430e11e4
 
 app.get('/hello', (req, res) => {
   res.send('Hello there!');
