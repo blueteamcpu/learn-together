e6b22826dcfd14a185522404b0f3bf2b82c44680
CB6670 <connerstennett@gmail.com>
2019-09-11 16:45:44 -0700

    shell of updatUserPass server route

--- server/apiRoutes/user/user.js
+++ server/apiRoutes/user/user.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 const {User} = require('../../db/index');
+const {makeHash} = require('../../../utils/backend')
 
 router.post('/updateUser', async (req, res, next) => {
     try {
@@ -19,6 +20,31 @@ router.post('/updateUser', async (req, res, next) => {
         res.send(newUser);
     }
 } catch (err){
+    next(err);
+}
+});
+
+router.post('/updateUserPass', async (req, res, next) => {
+    try {
+        const newUser = await User.update({
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
+        email: req.body.email,
+        zipcode: req.body.zipcode,
+        username: req.body.username,
+        password: req.body.NPass
+    }, {
+        returning: true,
+        where: {
+            id: req.session.userId,
+            password: makeHash(req.body.password),
+        }
+    });
+    if (newUser){
+        res.send(newUser);
+    }
+} catch (err){
+    res.send(err);
     next(err);
 }
 });
