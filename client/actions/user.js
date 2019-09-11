ae04a9f275587f9e5f86b113cb52ab5da4970e38
CB6670 <connerstennett@gmail.com>
2019-09-11 16:44:58 -0700

    user change password thunk and connection

--- client/actions/user.js
+++ client/actions/user.js
@@ -1,5 +1,6 @@
 //ACTION TYPES
 export const UPDATEUSER = 'UPDATEUSER';
+export const UPDATEUSERPASS = 'UPDATEUSERPASS';
 
 //ACTIONS
 
@@ -8,12 +9,27 @@ export const _updateUser = user => ({
     user,
   });
 
+export const _updateUserPass = user => ({
+    type: UPDATEUSERPASS,
+    user
+})
+
 // THUNKS
 
 export const updateUser = (firstName, lastName, username, email, zipcode) => async ( dispatch, _, axios) => {
     try {
       const user = {firstName, lastName, username, email, zipcode}
       const updatedUser = await axios.post('/user/updateUser', user)
+      dispatch(_updateUser(updatedUser));
+    } catch (err){
+      console.error(err);
+    }
+  };
+
+  export const updateUserPass = (firstName, lastName, username, email, zipcode, password, NPass) => async ( dispatch, _, axios) => {
+    try {
+      const user = {firstName, lastName, username, email, zipcode, password, NPass}
+      const updatedUser = await axios.post('/user/updateUserPass', user)
       dispatch(_updateUser(updatedUser));
     } catch (err){
       console.error(err);
--- client/components/UserProfile/UserProfile.js
+++ client/components/UserProfile/UserProfile.js
@@ -1,6 +1,6 @@
 import { connect } from 'react-redux';
 import UserProfileDumb from './UserProfileDumb';
-import {updateUser as _updateUser} from '../../actions/user';
+import {updateUser as _updateUser, updateUserPass as _updateUserPass} from '../../actions/user';
 import { getMe as _getMe } from '../../actions/authentication';
 
 const mapStateToProps = (state) => {
@@ -15,6 +15,9 @@ const mapStateToProps = (state) => {
 const mapDispatchToProps = (dispatch, {history}) => ({
   updateUser(firstName, lastName, username, email, zipcode) {
     dispatch(_updateUser(firstName, lastName, username, email, zipcode));
+  },
+  updateUserPass(firstName, lastName, username, email, zipcode, password, NPass) {
+    dispatch(_updateUserPass(firstName, lastName, username, email, zipcode, password, NPass))
   },
   getUser() {
     dispatch(_getMe());
