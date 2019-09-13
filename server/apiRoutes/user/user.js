3f31e451e5d143f8c346f1b68ca7521c9bafbbc5
CB6670 <connerstennett@gmail.com>
2019-09-13 16:40:34 -0700

    updating req.user instaed of User model

--- server/apiRoutes/user/user.js
+++ server/apiRoutes/user/user.js
@@ -4,23 +4,14 @@ const {compareStrAgainstHash} = require('../../../utils/index');
 
 router.put('/updateUser', async (req, res, next) => {
     try {
-        const newUser = await User.update({
+        const newUser = await req.user.update({
         firstName: req.body.firstName,
         lastName: req.body.lastName,
         email: req.body.email,
         zipcode: req.body.zipcode,
         username: req.body.username
-    }, {
-        returning: true,
-        where: {
-            id: req.session.userId
-        }
     });
-    if (newUser !== [0]){
-        res.send(newUser[1][0]);
-    } else {
-        res.send('No User Found')
-    }
+    res.send(newUser);
 } catch (err){
     next(err);
 }
@@ -29,16 +20,12 @@ router.put('/updateUser', async (req, res, next) => {
 router.put('/updateUserPass', async (req, res, next) => {
     try {
         const validation = await compareStrAgainstHash(req.body.password, req.user.password);
-        console.log('HIT IN UPDATEUSERPASS')
-        console.log(validation)
-        if (validation && req.user.id === req.session.userId){
-            console.log('HIT IN VALIDATION')
+        if (validation){
             await req.user.update({
             password: req.body.NPass
             });
             res.sendStatus(204);
         } else {
-            console.log('FAILED')
             res.sendStatus(401);
         }
     } catch (err){
