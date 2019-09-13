d7622b7fa7c00fdc90d46534b5e7a6e99abf7038
CB6670 <connerstennett@gmail.com>
2019-09-12 20:23:37 -0700

    get rid of password update thunk

--- client/actions/user.js
+++ client/actions/user.js
@@ -1,3 +1,5 @@
+import { log } from "util";
+
 //ACTION TYPES
 export const UPDATEUSER = 'UPDATEUSER';
 export const UPDATEUSERPASS = 'UPDATEUSERPASS';
@@ -9,27 +11,13 @@ export const _updateUser = user => ({
     user,
   });
 
-export const _updateUserPass = user => ({
-    type: UPDATEUSERPASS,
-    user
-})
-
 // THUNKS
 
 export const updateUser = (firstName, lastName, username, email, zipcode) => async ( dispatch, _, axios) => {
     try {
       const user = {firstName, lastName, username, email, zipcode}
-      const updatedUser = await axios.post('/user/updateUser', user)
-      dispatch(_updateUser(updatedUser));
-    } catch (err){
-      console.error(err);
-    }
-  };
-
-  export const updateUserPass = (firstName, lastName, username, email, zipcode, password, NPass) => async ( dispatch, _, axios) => {
-    try {
-      const user = {firstName, lastName, username, email, zipcode, password, NPass}
-      const updatedUser = await axios.post('/user/updateUserPass', user)
+      const updatedUser = await axios.put('/user/updateUser', user)
+      console.log('updated user: ', updatedUser);
       dispatch(_updateUser(updatedUser));
     } catch (err){
       console.error(err);
