1b3cbfa3ed2c64de0db4cf3efb63f5b8d6b3cd3b
CB6670 <connerstennett@gmail.com>
2019-09-13 14:32:13 -0700

    using gotUser instead of updateUser

--- client/actions/user.js
+++ client/actions/user.js
@@ -1,13 +1,6 @@
-//ACTION TYPES
-export const UPDATEUSER = 'UPDATEUSER';
-export const UPDATEUSERPASS = 'UPDATEUSERPASS';
-
 //ACTIONS
 
-export const _updateUser = user => ({
-    type: UPDATEUSER,
-    user,
-  });
+import { gotUser } from "./authentication";
 
 // THUNKS
 
@@ -15,8 +8,7 @@ export const updateUser = (firstName, lastName, username, email, zipcode) => asy
     try {
       const user = {firstName, lastName, username, email, zipcode}
       const updatedUser = await axios.put('/user/updateUser', user)
-      console.log('updated user: ', updatedUser);
-      dispatch(_updateUser(updatedUser));
+      dispatch(gotUser(updatedUser));
     } catch (err){
       console.error(err);
     }
