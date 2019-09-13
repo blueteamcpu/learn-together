e083132e08d8a463fc09dc9aa7562c6e0b1488ff
CB6670 <connerstennett@gmail.com>
2019-09-12 20:21:51 -0700

    add functional password update route

--- server/apiRoutes/user/user.js
+++ server/apiRoutes/user/user.js
@@ -1,8 +1,8 @@
 const router = require('express').Router();
 const {User} = require('../../db/index');
-const {makeHash} = require('../../../utils/backend')
+const {compareStrAgainstHash} = require('../../../utils/index');
 
-router.post('/updateUser', async (req, res, next) => {
+router.put('/updateUser', async (req, res, next) => {
     try {
         const newUser = await User.update({
         firstName: req.body.firstName,
@@ -16,37 +16,34 @@ router.post('/updateUser', async (req, res, next) => {
             id: req.session.userId
         }
     });
-    if (newUser){
-        res.send(newUser);
+    if (newUser !== [0]){
+        res.send(newUser[1][0]);
+    } else {
+        res.send('No User Found')
     }
 } catch (err){
     next(err);
 }
 });
 
-router.post('/updateUserPass', async (req, res, next) => {
+router.put('/updateUserPass', async (req, res, next) => {
     try {
-        const newUser = await User.update({
-        firstName: req.body.firstName,
-        lastName: req.body.lastName,
-        email: req.body.email,
-        zipcode: req.body.zipcode,
-        username: req.body.username,
-        password: req.body.NPass
-    }, {
-        returning: true,
-        where: {
-            id: req.session.userId,
-            password: makeHash(req.body.password),
+        const validation = await compareStrAgainstHash(req.body.password, req.user.password);
+        console.log('HIT IN UPDATEUSERPASS')
+        console.log(validation)
+        if (validation && req.user.id === req.session.userId){
+            console.log('HIT IN VALIDATION')
+            await req.user.update({
+            password: req.body.NPass
+            });
+            res.sendStatus(204);
+        } else {
+            console.log('FAILED')
+            res.sendStatus(401);
         }
-    });
-    if (newUser){
-        res.send(newUser);
+    } catch (err){
+        next(err);
     }
-} catch (err){
-    res.send(err);
-    next(err);
-}
 });
 
 module.exports = router;
