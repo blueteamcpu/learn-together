9b7b187cacfcfb7b38ad872eb6868701c2b8bf22
CB6670 <connerstennett@gmail.com>
2019-09-13 16:41:08 -0700

    send correct data from updatUser thunk

--- client/actions/user.js
+++ client/actions/user.js
@@ -1,13 +1,13 @@
 //ACTIONS
 
-import { gotUser } from "./authentication";
+import { gotUser } from './authentication';
 
 // THUNKS
 
 export const updateUser = (firstName, lastName, username, email, zipcode) => async ( dispatch, _, axios) => {
     try {
       const user = {firstName, lastName, username, email, zipcode}
-      const updatedUser = await axios.put('/user/updateUser', user)
+      const {data: updatedUser} = await axios.put('/user/updateUser', user)
       dispatch(gotUser(updatedUser));
     } catch (err){
       console.error(err);
