78ffb63807ac2bf2f40d12837f94ed45df3efbfa
CB6670 <connerstennett@gmail.com>
2019-09-16 18:30:03 -0700

    created groupPosts route

---
+++ server/apiRoutes/post.js
@@ -0,0 +1,19 @@
+const router = require('express').Router();
+//const { queryForUser } = require('../../utils/backend');
+// const { client, cacheDuration } = require('../redis');
+const { Post } = require('../db/index');
+
+router.get('/groupPosts', async (req, res, next) => {
+    try {
+        const posts = await Post.findAll({
+            where: {
+                groupId: req.params.groupId
+            }
+        });
+        res.json(posts);
+    } catch (err) {
+        next(err);
+    }
+});
+
+module.exports = router;
