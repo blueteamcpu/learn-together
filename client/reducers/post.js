74e6c5807d8f9a96a91c11a41aa147bcaf205872
CB6670 <connerstennett@gmail.com>
2019-09-18 13:57:24 -0700

    added failsedtoremovepost case

--- client/reducers/post.js
+++ client/reducers/post.js
@@ -1,24 +1,30 @@
 import {
     GETPOST,
     FAILEDTOLOADPOST,
+    REMOVEPOST,
+    FAILEDTOREMOVEPOST,
 } from '../actions/post';
 
 const initialState = {
     currentPost: {},
     failedToLoadPost: false,
+    failedToRemoveUser: false,
     loadingPost: true,
 };
 
 export default (state = initialState, action) => {
-    switch (action.type){
-        case GETPOST:{
-            return {...state, currentPost: action.post, loadingPost: false};
+    switch (action.type) {
+        case GETPOST: {
+            return { ...state, currentPost: action.post, loadingPost: false };
         }
-        case REMOVEPOST:{
-            return {...state, currentPost: {}}
+        case REMOVEPOST: {
+            return { ...state, currentPost: {} }
         }
-        case FAILEDTOLOADPOST:{
-            return {...state, failedToLoadPost: true}
+        case FAILEDTOLOADPOST: {
+            return { ...state, failedToLoadPost: true }
+        }
+        case FAILEDTOREMOVEPOST: {
+            return { ...state, failedToRemoveUser: true }
         }
         default:
             return state
