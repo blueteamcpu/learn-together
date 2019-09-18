f543c6467fc11b08f456b6530a0aea7f31ac91e2
CB6670 <connerstennett@gmail.com>
2019-09-18 13:00:35 -0700

    basic post reducer

---
+++ client/reducers/post.js
@@ -0,0 +1,26 @@
+// import {
+//     GETPOST,
+//     FAILEDTOLOADPOST,
+// } from '../actions/post';
+
+const initialState = {
+    currentPost: {},
+    failedToLoadPost: false,
+    loadingPost: true,
+};
+
+export default (state = initialState, action) => {
+    switch (action.type){
+        case GETPOST:{
+            return {...state, currentPost: action.post, loadingPost: false};
+        }
+        case REMOVEPOST:{
+            return {...state, currentPost: {}}
+        }
+        case FAILEDTOLOADPOST:{
+            return {...state, failedToLoadPost: true}
+        }
+        default:
+            return state
+    }
+};
