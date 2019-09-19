0e222bc0ce39bb33cae8dc224357d46fef5aee8d
Justin Cook <justin.cook.dev@gmail.com>
2019-09-19 10:32:30 -0700

    Waiting for redis client to connect when starting app.

--- server/index.js
+++ server/index.js
@@ -1,9 +1,11 @@
 const app = require('./app/index');
 const { db } = require('./db/index');
+const { syncRedisClient } = require('./redis');
 
 const PORT = process.env.PORT || 3000;
 
-db.sync({ force: false })
+db.sync()
+  .then(() => syncRedisClient())
   .then(() => {
     app.listen(PORT, () => console.log(`App listening on port ${PORT}`));
   })
--- server/redis.js
+++ server/redis.js
@@ -1,13 +1,29 @@
 const { promisify } = require('util');
 const redis = require('redis');
 
+const cacheDuration = 1000 * 60 * 2;
 const REDIS_PORT = process.env.REDIS_PORT || 6379;
 
 const client = redis.createClient(REDIS_PORT);
-const cacheDuration = 1000 * 60 * 2;
 
+const onAsync = promisify(client.on).bind(client);
 const getAsync = promisify(client.get).bind(client);
 const setExAsync = promisify(client.setex).bind(client);
 const existsAsync = promisify(client.exists).bind(client);
 
-module.exports = { existsAsync, getAsync, setExAsync, cacheDuration };
+const syncRedisClient = async () => {
+  try {
+    await onAsync('connection');
+    return true;
+  } catch (error) {
+    throw error;
+  }
+};
+
+module.exports = {
+  syncRedisClient,
+  existsAsync,
+  getAsync,
+  setExAsync,
+  cacheDuration,
+};
