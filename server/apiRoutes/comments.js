57d15319ddf045db33ff88bd50ef561c302861aa
Justin Cook <justin.cook.dev@gmail.com>
2019-09-21 12:12:24 -0700

    Comment routes for post comment and thread.

--- server/apiRoutes/comments.js
+++ server/apiRoutes/comments.js
@@ -49,105 +49,132 @@ router.get('/thread/:id', async (req, res, next) => {
   }
 });
 
-router.post(
-  '/events/:eventId/comment/:commentId',
-  isLoggedIn,
-  (req, res, next) => {
-    try {
-      const io = req.app.get('io');
-      const room = `event-${req.params.eventId}`;
+router.post('/events/:eventId/comment/:commentId', isLoggedIn, (req, res) => {
+  const io = req.app.get('io');
+  const room = `event-${req.params.eventId}`;
 
-      const id = uuid();
+  const id = uuid();
 
-      Comment.commentOnAComment(
-        req.params.commentId,
-        req.body.content,
-        id,
-        req.user.id
-      ).catch(() => {
-        io.in(room).emit('message-thread-error', {
-          threadId: req.params.commentId,
-          id,
-        });
-      });
-
-      const initDate = new Date();
-
-      io.in(room).emit('message-thread', {
-        id,
-        content: req.body.content,
-        createdAt: initDate,
-        updatedAt: initDate,
-        threadId: req.params.commentId,
-        userId: req.user.id,
-        username: req.user.username,
-      });
-      
-      res.end();
-    } catch (error) {
-      next(error);
-    }
-  }
-);
-
-router.post('/events/:id', isLoggedIn, async (req, res, next) => {
-  try {
-    const io = req.app.get('io');
-
-    const comment = await Comment.create({
-      userId: req.user.id,
-      content: req.body.content,
-      eventId: req.params.id,
+  Comment.commentOnAComment(
+    req.params.commentId,
+    req.body.content,
+    id,
+    req.user.id
+  ).catch(() => {
+    io.in(room).emit('message-thread-error', {
+      threadId: req.params.commentId,
+      id,
     });
+  });
 
-    io.in(`event-${req.params.id}`).emit('message', comment);
-    res.end();
-  } catch (error) {
-    next(error);
-  }
+  const initDate = new Date();
+
+  io.in(room).emit('message-thread', {
+    id,
+    content: req.body.content,
+    createdAt: initDate,
+    updatedAt: initDate,
+    threadId: req.params.commentId,
+    userId: req.user.id,
+    username: req.user.username,
+  });
+
+  res.end();
 });
 
-router.post(
-  '/posts/:postId/comment/:commentId',
-  isLoggedIn,
-  async (req, res, next) => {
-    try {
-      const io = req.app.get('io');
+router.post('/events/:id', isLoggedIn, (req, res) => {
+  const io = req.app.get('io');
+  const room = `event-${req.params.id}`;
 
-      const comment = await Comment.commentOnAComment(
-        req.params.commentId,
-        req.body.content,
-        req.user.id
-      );
+  const id = uuid();
 
-      if (comment) {
-        io.in(`post-${req.params.postId}`).emit('message-thread', comment);
-        res.end();
-      } else {
-        const error = new Error('thread-depth > 1');
-        res.status(400).json(error);
-      }
-    } catch (error) {
-      next(error);
-    }
-  }
-);
+  Comment.create({
+    id,
+    userId: req.user.id,
+    content: req.body.content,
+    eventId: req.params.id,
+  }).catch(() => {
+    io.in(room).emit('message-error', { id });
+  });
 
-router.post('/posts/:id', isLoggedIn, async (req, res, next) => {
-  try {
-    const io = req.app.get('io');
+  const initDate = new Date();
 
-    const comment = await Comment.create({
-      userId: req.user.id,
-      content: req.body.content,
-      postId: req.params.id,
+  io.in(room).emit('message', {
+    id,
+    content: req.body.content,
+    createdAt: initDate,
+    updatedAt: initDate,
+    eventId: req.params.id,
+    userId: req.user.id,
+    username: req.user.username,
+  });
+
+  res.end();
+});
+
+router.post('/posts/:postId/comment/:commentId', isLoggedIn, (req, res) => {
+  const io = req.app.get('io');
+  const room = `post-${req.params.postId}`;
+
+  const id = uuid();
+
+  Comment.commentOnAComment(
+    req.params.commentId,
+    req.body.content,
+    id,
+    req.user.id
+  ).catch(() => {
+    io.in(room).emit('message-thread-error', {
+      threadId: req.params.commentId,
+      id,
     });
+  });
 
-    io.in(`post-${req.params.id}`).emit('message', comment);
-    res.end();
-  } catch (error) {
-    next(error);
-  }
+  const initDate = new Date();
+
+  io.in(room).emit('message-thread', {
+    id,
+    content: req.body.content,
+    createdAt: initDate,
+    updatedAt: initDate,
+    threadId: req.params.commentId,
+    userId: req.user.id,
+    username: req.user.username,
+  });
+
+  res.end();
+});
+
+router.post('/posts/:id', isLoggedIn, (req, res) => {
+  const io = req.app.get('io');
+  const room = `post-${req.params.id}`;
+
+  const id = uuid();
+
+  Comment.create({
+    id,
+    userId: req.user.id,
+    content: req.body.content,
+    postId: req.params.id,
+  }).catch(() => {
+    io.in(room).emit('message-error', {
+      id,
+    });
+  });
+
+  const initDate = new Date();
+
+  io.in(room).emit('message', {
+    id,
+    content: req.body.content,
+    createdAt: initDate,
+    updatedAt: initDate,
+    postId: req.params.id,
+    userId: req.user.id,
+    username: req.user.username,
+  });
+
+  res.end();
 });
 
 module.exports = router;
