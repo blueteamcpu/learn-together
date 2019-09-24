7574633aafbb865eb73d9d1729fcb4de95da4106
CB6670 <connerstennett@gmail.com>
2019-09-23 18:23:18 -0700

    added randomized posts to the seed

---
+++ seed/seedlings/postSeed.js
@@ -0,0 +1,35 @@
+const post = {
+    title: '',
+    description: '',
+};
+
+module.exports = [
+    {
+        title: 'Super Duper Post',
+        description: 'Super duper description'
+    },
+    {
+        title: 'Crazy Awesome Post',
+        description: 'Crazy awesome description'
+    },
+    {
+        title: 'Chill Post',
+        description: 'Chill description'
+    },
+    {
+        title: 'Dope Post',
+        description: 'Dope description'
+    },
+    {
+        title: 'Noice Post',
+        description: 'Noice description'
+    },
+    {
+        title: ':) Post',
+        description: ':) description'
+    },
+    {
+        title: ';) Post',
+        description: ';) description'
+    },
+];
