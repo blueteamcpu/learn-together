8d9491c04b3cd38c1b4530cf6671d6879b746491
CB6670 <connerstennett@gmail.com>
2019-09-23 18:22:17 -0700

    added mapStateToProps as well as remove post

--- client/components/Post/Post.js
+++ client/components/Post/Post.js
@@ -1,14 +1,22 @@
 import { connect } from 'react-redux';
-import { loadPost as _loadPost } from '../../actions/post';
+import { loadPost as _loadPost, removePost as _removePost } from '../../actions/post';
 import Post from './PostDumb';
+
+const mapStateToProps = ({posts, authentication}) => ({
+  currentPost: posts.currentPost,
+  user: authentication.user,
+})
 
 const mapDispatchToProps = (dispatch) => ({
   loadPost(postId) {
     dispatch(_loadPost(postId));
   },
+  removePost(postId) {
+    dispatch(_removePost(postId));
+  }
 });
 
 export default connect(
-  null,
+  mapStateToProps,
   mapDispatchToProps
 )(Post);
